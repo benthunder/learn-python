1828d25902d30a680eca7007d0ca3b937651cb7e
Lê Đình Thắng <ledinhthang102@gmail.com>
2023-05-24 13:35:51 +0000

    Init new spider

--- pytutorial/pytutorial/settings.py
+++ pytutorial/pytutorial/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "pytutorial.spiders"
 #USER_AGENT = "pytutorial (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
